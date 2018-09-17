8f32a0c3550e5f66aad958476d86789ad8c758ac
Tim Coutinho <tmcoutinho42@gmail.com>
2018-09-16 22:31:20 -0700

    Decimal allowed fix, added key listeners

--- src/App.js
+++ src/App.js
@@ -6,7 +6,80 @@ class App extends React.Component {
     super(props);
     this.state = {
       input: '0',
-      output: '0'
+      output: '0',
+      answered: false,
+      decimalAllowed: true,
+      lastAnswer: ''
+    };
+    this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown(e) {
+    switch(e.key) {
+        case('0'):
+          document.getElementById('zero').click();
+          break;
+        case('1'):
+          document.getElementById('one').click();
+          break;
+        case('2'):
+          document.getElementById('two').click();
+          break;
+        case('3'):
+          document.getElementById('three').click();
+          break;
+        case('4'):
+          document.getElementById('four').click();
+          break;
+        case('5'):
+          document.getElementById('five').click();
+          break;
+        case('6'):
+          document.getElementById('six').click();
+          break;
+        case('7'):
+          document.getElementById('seven').click();
+          break;
+        case('8'):
+          document.getElementById('eight').click();
+          break;
+        case('9'):
+          document.getElementById('nine').click();
+          break;
+        case('Backspace'):
+          document.getElementById('clear').click();
+          break;
+        case('/'):
+          document.getElementById('divide').click();
+          break;
+        case('x'):
+        case('X'):
+        case('*'):
+          document.getElementById('multiply').click();
+          break;
+        case('+'):
+          document.getElementById('add').click();
+          break;
+        case('-'):
+          document.getElementById('subtract').click();
+          break;
+        case('.'):
+          document.getElementById('decimal').click();
+          break;
+        case('Enter'):
+        case('='):
+          document.getElementById('equals').click();
+          break;
+        default:
+          break;
     };
   }
 
@@ -20,31 +93,52 @@ class App extends React.Component {
   }
 
   updateScreen(v) {
+    this.state.answered && this.setState({
+      input: '0',
+      output: '0',
+      answered: false
+    });
     switch(v) {
       case('AC'):
         this.setState({
           input: '0',
-          output: '0'
+          output: '0',
+          decimalAllowed: true
         });
         break;
       case('='):
+        const answer = eval(this.state.input.replace('X', '*'));
+        this.setState({
+          input: this.state.input + '=' + answer,
+          output: answer,
+          answered: true,
+          lastAnswer: '' + answer
+        });
         break;
       case('.'):
-        !this.state.input.includes(v) && !this.outputTooLong() && this.setState({
+        this.state.decimalAllowed && !this.outputTooLong() && this.setState({
           input: this.state.input + v,
-          output: this.state.output + v
+          output: this.state.output + v,
+          decimalAllowed: false
         });
         break;
       case('+'):
       case('-'):
       case('X'):
       case('/'):
+        const lastAnswer = this.state.lastAnswer;
+        lastAnswer !== '' && this.setState({
+          input: lastAnswer,
+          lastAnswer: ''
+        });
         ['+', '-', 'X', '/'].some((x) => x === this.state.input.slice(-1)) ? this.setState({
           input: this.state.input.slice(0, -1) + v,
-          output: v
+          output: v,
+          decimalAllowed: true
         }) : this.setState({
           input: this.state.input + v,
-          output: v
+          output: v,
+          decimalAllowed: true
         });
         break;
       default:
